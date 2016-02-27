32948e93d370e3124135b00dc34fb8e4f7120186
Stealthmate <stealthmate1233@gmail.com>
2016-02-27 14:01:53 +0200

    Added hotkeys for selecting ReactantLabel

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -2,65 +2,84 @@ package gui;
 
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.KeyboardFocusManager;
 import java.awt.Toolkit;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.KeyEvent;
+import java.awt.KeyEventDispatcher;
 
 import javax.swing.JFrame;
 
 import gui.mainscreen.GlobalPanel;
+import gui.mainscreen.reactionpanel.ReactantLabel;
 
 public class MainFrame extends JFrame {
 
 	private static int WIDTH;
 	private static int HEIGHT;
-	
+
 	private static GlobalPanel pnlGlobal;
-	
+
 	public static final Font MAIN_FONT = new Font("Arial", Font.PLAIN, 15);
-	
+	public static final Dimension DEFAULT_SIZE = new Dimension(800, 600);
+
 	public static int getFrameWidth() {
 		return WIDTH;
 	}
-	
+
 	public static int getFrameHeight() {
 		return HEIGHT;
 	}
-	
+
 	public static int getScreenWidth() {
 		return (int) Toolkit.getDefaultToolkit().getScreenSize().width;
 	}
-	
+
 	public static int getScreenHeight() {
 		return (int) Toolkit.getDefaultToolkit().getScreenSize().height;
 	}
-	
+
 	public void resizeLayout(Dimension d) {
 		pnlGlobal.resizeLayout(d);
 	}
-	
+
 	public MainFrame(int width, int height, String name) {
 		super(name);
 
+
 		WIDTH = width;
 		HEIGHT = height;
-		
+
 		setSize(new Dimension(width, height));
 		pnlGlobal = new GlobalPanel(width, height);
 		this.add(pnlGlobal);
 		this.invalidate();
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.addComponentListener(new ComponentAdapter() {
-			
+
 			@Override
 			public void componentResized(ComponentEvent e) {
 				resizeLayout(e.getComponent().getSize());
 			}
-			
+
 		});
-		
-		}
-	
-	
-	
+
+		KeyboardFocusManager.getCurrentKeyboardFocusManager()
+		.addKeyEventDispatcher(new KeyEventDispatcher() {
+			@Override
+			public boolean dispatchKeyEvent(KeyEvent e) {
+				if(e.isControlDown()) {
+					switch (e.getKeyCode()) {
+					case KeyEvent.VK_1 : pnlGlobal.selectReactant(0); break;
+					case KeyEvent.VK_2 : pnlGlobal.selectReactant(1); break;
+					case KeyEvent.VK_3 : pnlGlobal.selectReactant(2); break;
+					}
+				}
+				return false;
+			}
+		});
+
+	}
+
 }
